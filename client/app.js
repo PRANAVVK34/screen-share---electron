b09ef83c0c5a53d4646eb561b9a3fc5710b85e39
PRANAVVK34 <pranav@volcano.social>
2021-05-26 16:40:07 +0530

    screen share using electron

---
+++ client/app.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const screenshot = require("screenshot-desktop");
+
+var socket = require("socket.io-client")("http://192.168.1.6:5000");
+var interval;
+
+process.env.NODE_ENV = "production";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 150,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  win.removeMenu();
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+ipcMain.on("start-share", function (event, arg) {
+  var uuid = "pranav";
+  socket.emit("join-message", uuid);
+  event.reply("uuid", uuid);
+
+  interval = setInterval(function () {
+    screenshot().then((img) => {
+      var imgStr = new Buffer(img).toString("base64");
+
+      var obj = {};
+      obj.room = uuid;
+      obj.image = imgStr;
+
+      socket.emit("screen-data", JSON.stringify(obj));
+    });
+  }, 100);
+});
+
+ipcMain.on("stop-share", function (event, arg) {
+  clearInterval(interval);
+});
